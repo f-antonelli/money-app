98a74f379ad47e04e80364ec6641ee632c4a9429
f-antonelli <antonelli.fd@gmail.com>
2023-07-18 16:46:02 -0300

    ADD: connection to postgresql

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,20 @@
 import { app } from './helpers/app';
+import { PostgresDataSource } from './helpers/db-connection';
+
+const start = () => {
+  if (!process.env.NODE_ENV) throw new Error('NODE_ENV must be defined');
+};
+
+PostgresDataSource.initialize()
+  .then(() => {
+    console.log('Connected to PostgreSQL');
+  })
+  .catch((err: Error) => {
+    console.error('Error during Data Source initialization', err);
+  });
 
 app.listen(3000, () => {
   console.log('Application is running on port 3000.');
 });
+
+start();
